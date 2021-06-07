5c924b752802706c4c826573b37deacbb910b7df
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2021-06-07 19:34:21 +0530

    Create user_ip.py

---
+++ user_ip.py
@@ -0,0 +1,9 @@
+#Basic sum
+print(2000+19)
+#other prg
+name=input("What is your name?")
+color=input("What is your fav color?")
+print(name)
+print(color)
+print("Hi "+ name +"!" + color +" is a great color!")
+
